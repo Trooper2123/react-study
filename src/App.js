fb6487339f6cfa32bfb99dfb62864d1b9ae458c3
Trooper2123 <tete.leticia@gmail.com>
2021-02-09 18:58:16 -0300

    Using jsx and introduction to hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import * as React from 'react';
 import {render} from 'react-dom';//import just this pack
-import Pet from "./Pet";
-//import ReactDOM from 'react-dom //import all
+//import ReactDOM from 'react-dom //
+import SearchParams from "./SearchParams";
 
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {id: "important"},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {name: "Mel", animal: "Dog", breed: "SRD"}),
-            React.createElement(Pet, {name: "Mini", animal: "Dog", breed: "SRD"}),
-            React.createElement(Pet, {name: "Nina", animal: "Dog", breed: "SRD"}),
-            React.createElement(Pet, {name: "Doink", animal: "Cat", breed: "SRD"})
-        ]
+    return (
+        <div>
+            <h1 id="important">Adopt Me!</h1>
+            <SearchParams/>
+        </div>
     );
 };
 
-render(React.createElement(App),
-    document.getElementById("root")
-);
+render(<App/>,
+    document.getElementById("root"));
