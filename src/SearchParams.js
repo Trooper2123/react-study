482f7f7fc614a517b9c5a65f3d9abeacce8f648d
Trooper2123 <tete.leticia@gmail.com>
2021-02-16 18:12:34 -0300

    create dropdown element for animals and breeds;
    create custom hook for dropdown;

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { ANIMALS } from "@frontendmasters/pet";
+
 
 const SearchParams =() => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal,setAnimal] = useState("dog");
+    const [breed,setBreed] = useState("");
+    const [breeds,setBreeds] = useState([]);
+
     return(
         <div className="search-params">
             <form>
@@ -11,6 +17,32 @@ const SearchParams =() => {
                        placeholder="location"
                        onChange={ e => setLocation(e.target.value)}
                 />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}>
+                        <option>All</option>
+                        {ANIMALS.map(animal => (
+                            <option key={animal} value={animal}>{animal}</option>))}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        id="breed"
+                        value={breed}
+                        onChange={e => setBreed(e.target.value)}
+                        onBlur={e => setBreed(e.target.value)}
+                        disabled={!breeds.length}
+                    >
+                        <option>All</option>
+                        {breeds.map(breedString => (
+                            <option key={breedString} value={breedString}>{breedString}</option>))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
