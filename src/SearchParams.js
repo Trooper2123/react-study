008a61136ee5c952b7037f58403542affddc5e11
Trooper2123 <tete.leticia@gmail.com>
2021-02-16 18:48:58 -0300

    render and change breeds according to animal selected

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
-import { ANIMALS } from "@frontendmasters/pet";
+import React, {useState, useEffect} from 'react';
+import pet, {ANIMALS} from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 
-const SearchParams =() => {
+const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
-    const [breeds,setBreeds] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("Animal","dog",ANIMALS);
-    const [breed,BreedDropdown] = useDropdown("Breed","",breeds);
+    const [breeds, setBreeds] = useState([]);
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
 
-    return(
+    useEffect(() => {
+        setBreeds([]);
+        setBreed("");
+        pet.breeds(animal).then(({ breeds })=> {
+            const breedStrings = breeds.map(({ name })=> name);
+            setBreeds(breedStrings);
+        },console.error);
+    },[animal, setBreeds, setBreed]);
+
+    return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     Location
-                <input id="location" value={location}
-                       placeholder="location"
-                       onChange={ e => setLocation(e.target.value)}
-                />
+                    <input id="location" value={location}
+                           placeholder="location"
+                           onChange={e => setLocation(e.target.value)}
+                    />
                 </label>
-                <AnimalDropdown />
+                <AnimalDropdown/>
                 <BreedDropdown/>
                 <button>Submit</button>
             </form>
