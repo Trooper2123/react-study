9fed887f6d2e393a61b620b26bbf16deac603008
Trooper2123 <tete.leticia@gmail.com>
2021-02-18 14:09:14 -0300

    render and change breeds according to animal selected

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import {render} from 'react-dom';//import just this pack
+import { Router } from "@reach/router";
 //import ReactDOM from 'react-dom //
 import SearchParams from "./SearchParams";
 
 
 const App = () => {
     return (
+        <React.StrictMode>
         <div>
             <h1 id="important">Adopt Me!</h1>
             <SearchParams/>
         </div>
+        </React.StrictMode>
     );
 };
 
